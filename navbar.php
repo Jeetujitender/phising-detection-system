564047fbcb3210ec84de3b36035de39230e10369
Jeetujitender <39960180+Jeetujitender@users.noreply.github.com>
2020-04-08 22:39:43 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -125,7 +125,7 @@ h1{
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#"><i class="fa fa-university" aria-hidden="true"></i>Bank Card</a>
+      <a class="navbar-brand" href="#"><i class="fa fa-university" aria-hidden="true"></i>Phising Detection</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
